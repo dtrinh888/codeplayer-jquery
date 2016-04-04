da55b505c110efe295fdf75cbd011b5de4bbb1a2
dtrinh888 <dtrinh888@gmail.com>
2016-04-03 19:09:09 -0700

    toggling finished

--- js/app.js
+++ js/app.js
@@ -4,4 +4,22 @@ $(function(){
 	var codeContainerHeight = windowHeight - menuBarHeight;
 
 	$('.code-container').height(codeContainerHeight + 'px');
+
+	$('.toggle').click(function(){
+		$(this).toggleClass('toggle-selected');
+
+		var activeDiv = $(this).html();
+
+		$('#'+activeDiv.toLowerCase()+'-container').toggle();
+
+		// function for length of selected divs by filtering
+		// all css elements with display value not equal to none
+		var showingDivs = $('.code-container').filter(function(){
+			return($(this).css('display')!="none");
+		}).length;
+
+		var windowWidth = 100/showingDivs;
+
+		$('.code-container').css('width', windowWidth+'%');
+	});
 });
